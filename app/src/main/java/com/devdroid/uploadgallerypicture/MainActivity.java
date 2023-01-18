8553f3966b9f35acebecb9a02e415c2ff71650a1
Talha Waris <victormalik60@gmail.com>
2023-01-18 08:55:28 +0500

    Initial Commit

---
+++ app/src/main/java/com/devdroid/uploadgallerypicture/MainActivity.java
@@ -0,0 +1,51 @@
+package com.devdroid.uploadgallerypicture;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private final int Gallery_REQ_CODE   = 100;
+
+    ImageView view;
+    Button button;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.galleryImg);
+        button = findViewById(R.id.Btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery,Gallery_REQ_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK)
+        {
+            if (requestCode == Gallery_REQ_CODE)
+            {
+                // For gallery
+                view.setImageURI(data.getData());
+            }
+        }
+    }
+}
